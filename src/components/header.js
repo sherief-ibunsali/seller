625a5ecabeb34a242474d3278180c6bd60abd7a5
sherief-ibunsali <mdsheriefibunsali@gmail.com>
2024-10-10 00:32:14 +0530

    files added

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="app-header">
       <img src={`${process.env.PUBLIC_URL}/img/js.png`} alt="img" />
-      <h1>The JS Quiz</h1>
+      <h1>THE JS QUIZ</h1>
     </header>
   );
 }
